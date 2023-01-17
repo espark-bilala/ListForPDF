74239eb18e606f82dc7a95fae01332be4c4289e4
Bllal Ahmed Khan <98737151+espark-bilala@users.noreply.github.com>
2023-01-17 19:52:44 +0500

    useless line of code

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,6 @@
     </div>
     <div>
   </div>
-    <!-- <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav> -->
-    <!-- <router-view /> -->
   </div>
 </template>
 
